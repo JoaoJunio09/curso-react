5624782fc9a7b1794cbfd3bbc1024963cf2fa9fb
JoaoJunio09 <joaojunio818@gmail.com>
2026-04-30 15:03:26 -0300

    Finalizando projetos de gerador de frases

--- dev-frases/src/App.tsx
+++ dev-frases/src/App.tsx
@@ -1,8 +1,55 @@
+import { useState } from 'react';
 import './App.css'
 
 import logoImg from './assets/logo.png';
 
 function App() {
+  const [textoFrase, setTextoFrase] = useState('');
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState(0);
+
+  const allFrases = [
+    {
+      id: 1,
+      nome: 'Motivação',
+      frases: [
+        'Siga os bons e aprenda com eles.',
+        'O bom-senso vale mais do que muito conhecimento',
+        'O riso é a menor distância entre duas pessoas.',
+        'Deixe de lado as preocupações e seja feliz',
+        'Realize o óbvio, pense no improvável e conquista o impossível',
+        'Acredite em milagres, mas não dependa deles',
+        'A maior barreira para o sucesso é o medo do fracasso.'
+      ]
+    },
+    {
+      id: 2,
+      nome: 'Bom dia',
+      frases: [
+        'Acordar de bem com a vida é o primeiro passo para ter um dia abençoado! Bom dia, família!',
+        'A melhor forma de acordar é pular da cama e se preparar para correr atrás de todos os seus sonhos! Bom dia, mundo!',
+        'Escreva em seu coração: todo dia é o melhor dia do ano',
+        'Bom dia! Não se esqueça que sua alma é o reflexo do sol, tão brilhante quanto um girassol.'
+      ]
+    },
+    {
+      id: 3,
+      nome: 'Boa noite',
+      frases: [
+        'Boa noite durma bem',
+        'Teste frase boa noite'
+      ]
+    }
+  ];
+
+  function handleSwitchCategory(index: number) {
+    setCategoriaSelecionada(index);
+  }
+
+  function gerarFrase() {
+    let numeroAleatorio = Math.floor(Math.random() * allFrases[categoriaSelecionada].frases.length)
+    setTextoFrase(`"${allFrases[categoriaSelecionada].frases[numeroAleatorio]}"`);
+  }
+
   return (
     <div className='container'>
       <img 
@@ -13,13 +60,25 @@ function App() {
 
       <h2 className='title'>Categorias</h2>
       <section className='category-area'>
-        <button className='category-button'>Motivação</button>
-        <button className='category-button'>Bem estar</button>
+        {allFrases.map((item, index) => (
+          <button 
+            key={item.id}
+            className='category-button'
+            style={{ 
+              borderWidth: item.nome === allFrases[categoriaSelecionada].nome ? 2 : 0,
+              borderColor: '#1fa4db'
+             }}
+
+            onClick={() => handleSwitchCategory(index)}
+          >
+            {item.nome}
+           </button>
+        ))}
       </section>
 
-      <button className='button-frase'>Gerar Frase</button>
+      <button className='button-frase' onClick={gerarFrase}>Gerar Frase</button>
 
-      <p className='textoFrase'>Alguma frase vai vir aqui</p>
+      {textoFrase !== '' && <p className='textoFrase'>{textoFrase}</p>}
 
     </div>
   )
