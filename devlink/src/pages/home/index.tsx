5a126f3a98b5f8665b71fd4a8d6768b5126205ce
JoaoJunio09 <joaojunio818@gmail.com>
2026-05-06 23:10:45 -0300

    Componentizando icons de Socail e inciando implementação de tela de login

--- devlink/src/pages/home/index.tsx
+++ devlink/src/pages/home/index.tsx
@@ -1,22 +1,34 @@
+import { FaAmilia, FaFacebook, FaInstagram, FaLink, FaSitemap, FaYoutube } from "react-icons/fa";
+import { Social } from "../../components/Social";
+import { FaWebAwesome, FaWebflow, FaWeibo } from "react-icons/fa6";
+
 export function Home() {
 	return (
 		<div className="flex flex-col w-full py-4 items-center justify-center">
 			<h1 className="md:text-4xl text-3xl font-bold text-white mt-20">João Junio Dev</h1>
 			<span className="text-gray-50 mb-5 mt-3">Veja meus links</span>
 
-		<main className="flex flex-col w-11/12 max-w-xl text-center">
-			<section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105">
-				<a>
-					<p className="text-base md:text-lg">
-						Canal no Youtube
-					</p>
-				</a>
-			</section>
+			<main className="flex flex-col w-11/12 max-w-xl text-center">
+				<section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105">
+					<a>
+						<p className="text-base md:text-lg">
+							Canal no Youtube
+						</p>
+					</a>
+				</section>
 
-			<footer className="flex justify-center gap-3 my-4">
-				
-			</footer>
-		</main>
+				<footer className="flex justify-center gap-3 my-4">
+					<Social url="https://spc-azure.vercel.app">
+						<FaLink size={35} color="#fff" />
+					</Social>
+					<Social url="https://youtube.com/sujeitoprogramador">
+						<FaYoutube size={35} color="#fff" />
+					</Social>
+					<Social url="https://instagram.com/joaojunio_09">
+						<FaInstagram size={35} color="#fff" />
+					</Social>
+				</footer>
+			</main>
 
 		</div>
 	)
--- devlink/src/pages/login/index.tsx
+++ devlink/src/pages/login/index.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export function Login() {
 	return (
-		<div>
-			Página Login
+		<div className="flex flex-col w-full h-screen items-center justify-center">
+			<Link to="/home">
+				<h1 className="mt-11 text-white mb-7 font-bold text-5xl">Dev
+				<span className="bg-linear-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">Link</span>
+				</h1>
+			</Link>
 		</div>
 	)
 }
