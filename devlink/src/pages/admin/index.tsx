d31ba4eb2ca2133fbef5da83723b5d9a24dd09d5
JoaoJunio09 <joaojunio818@gmail.com>
2026-05-15 15:34:53 -0300

    Finalizando formulário

--- devlink/src/pages/admin/index.tsx
+++ devlink/src/pages/admin/index.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 
+import { FiTrash } from 'react-icons/fi';
+
 export function Admin() {
 	const [nameInput, setNameInput] = useState('');
 	const [urlInput, setUrlInput] = useState('');
@@ -47,16 +49,42 @@ export function Admin() {
 					</div>
 				</section>
 
-				<div className="flex items-center  justify-start flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
-					<label className="text-white font-medium mt-2 mb-3">Veja como está ficando:</label>
-					<article
-						className="w-11/12 max-w-lg flex flex-col justify-between bg-zinc-900 rounded px-1 py-3"
-						style={{ marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput }}
-					>
-						<p className="font-medium" style={{ color: textColorInput }}>Canal do Youtube</p>
-					</article>
-				</div>
+				{nameInput !== '' && (
+					<div className="flex items-center  justify-start flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
+						<label className="text-white font-medium mt-2 mb-3">Veja como está ficando:</label>
+						<article
+							className="w-11/12 max-w-lg flex flex-col justify-between bg-zinc-900 rounded px-1 py-3"
+							style={{ marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput }}
+						>
+							<p className="font-medium" style={{ color: textColorInput }}>{nameInput}</p>
+						</article>
+					</div>
+				)}
+
+				<button type="submit" className="mb-7 bg-blue-600 h-9 rounded-md text-white font-medium gap-4 flex justify-center items-center">
+					Cadastrar
+				</button>
+
 			</form>
+
+			<h2 className="font-bold text-white mb-4 text-xl">
+				Meus Links
+			</h2>
+
+			<article
+				className="flex item-center justify-between w-11/12 max-w-xl rounded py-3 px-2 mb-2 select-none"
+				style={{ backgroundColor: '#2563eb', color: '#fff' }}
+			>
+				<p>Canal do youtube</p>
+				<div>
+					<button
+						className="border border-dashed p-1 rounded bg-neutral-900"
+					>
+						<FiTrash size={18} color="#fff" />
+					</button>
+				</div>
+			</article>
+
 		</div>
 	)
 }
